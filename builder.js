88a5b2e5a384d7c71e9ef414dc39d79980b49c24
winton <mail@wintoni.us>
2020-02-15 18:24:37 -0800

    First commit

---
+++ builder.js
@@ -0,0 +1,31 @@
+const boiler = require("boiler-dev")
+
+module.exports.setupBoiler = function ({ destDir }) {
+  const pkgNames = [
+    "typescript",
+    "@typescript-eslint/eslint-plugin",
+    "@typescript-eslint/parser"
+  ]
+  return boiler
+    .npm({ destDir, pkgNames })
+    .then(function ({ code, out }) {
+      if (code === 1) {
+        console.log("✅ Installed npm modules:", pkgNames)
+      } else {
+        console.error("🚨 Failed to install npm modules:", pkgNames)
+        console.error(out)
+      }
+    })
+}
+
+module.exports.installBoiler = function ({ destDir, files }) {
+  return files.map(function (file) {
+    if (file.name === "tsconfig.base.json") {
+      return {
+        action: "write",
+        path: join(destDir, file.name),
+        source: file.source,
+      }
+    }
+  })
+}
